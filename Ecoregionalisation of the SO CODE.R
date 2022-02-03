9f282bb35f7ebf5bcd0db75fa4be95a6578b6af4
Vikki Lowe <70927381+vloweSO@users.noreply.github.com>
2022-02-04 09:01:08 +1000

    Update Ecoregionalisation of the SO CODE.R
    Edited code to conduct MRT analysis first, then write MRT clusters to "factors.csv" files.

--- Ecoregionalisation of the SO CODE.R
+++ Ecoregionalisation of the SO CODE.R
@@ -37,6 +37,66 @@ spe.hel <- decostand(spe, "hellinger")
 spe.norm <- decostand(spe, "normalize")
 
 #==========================================================================================================================================
+# MRT : CONSTRAINED CLUSTERING
+#==========================================================================================================================================
+
+dev.new(
+  title = "Multivariate regression tree - all explanatory variables - ALL",
+  width = 14,
+  height = 7,
+  noRStudioGD = TRUE
+)
+par(mfrow = c(1, 2))
+spe.ch.mvpart <-
+  mvpart(
+    data.matrix(spe.norm) ~ .,
+    env,
+    margin = 0.08,
+    cp = 0,
+    xv = "pick",
+    xval = nrow(spe),
+    xvmult = 100
+  )
+
+summary(spe.ch.mvpart)
+printcp(spe.ch.mvpart)
+
+dev.new(
+  title = "Residuals of MRT",
+  width = 10,
+  height = 6,
+  noRStudioGD = TRUE
+)
+par(mfrow = c(1, 2))
+hist(residuals(spe.ch.mvpart), col = "bisque")
+plot(predict(spe.ch.mvpart, type = "matrix"),
+     residuals(spe.ch.mvpart),
+     main = "Residuals vs Predicted")
+abline(h = 0, lty = 3, col = "grey")
+
+
+spe.ch.mvpart.wrap <-
+  MRT(spe.ch.mvpart, percent = 10, species = colnames(spe))
+summary(spe.ch.mvpart.wrap)
+spe.ch.MRT.indval <- indval(spe.norm, spe.ch.mvpart$where)
+pval.adj3 <- p.adjust(spe.ch.MRT.indval$pval)
+spe.ch.MRT.indval$maxcls[which(pval.adj3 <= 0.05)]
+spe.ch.MRT.indval$indcls[which(pval.adj3 <= 0.05)]
+spech.mvpart.g <- factor(spe.ch.mvpart$where)
+levels(spech.mvpart.g) <- 1:length(levels(spech.mvpart.g))
+table(spech.mvpart.g, spech.UPGMA.g)
+
+dev.new(title = "MRT clusters",
+        width = 9,
+        noRStudioGD = TRUE)
+drawmap(xy = spa,
+        clusters = spech.mvpart.g,
+        main = "MRT clusters")
+
+MRTclust <- as.data.frame(spech.mvpart.g)
+MRTclust <- mutate(MRTclust, data = seq(n()))
+
+#==========================================================================================================================================
 # NMDS
 #==========================================================================================================================================
 
@@ -45,19 +105,22 @@ Data.scores = as.data.frame(scores(datanMDS))
 Data.scores = mutate(Data.scores, data = seq(n()))
 factors.scores = mutate(factors, data = seq(n()))
 Data.scores = right_join(factors.scores, Data.scores)
+Data.scores = right_join(Data.scores, MRTclust)
 datanMDS$stress <- round(datanMDS$stress, 4)
-
 Data.scores$MRTClust <- as.factor(Data.scores$MRTClust)
+
+
 plot = ggplot() +
-  geom_point(data=Data.scores, aes(x=NMDS1, y=NMDS2, shape = Zone, colour = Sector), size = 3) +
+  geom_point(data=Data.scores, aes(x=NMDS1, y=NMDS2, shape = Zone, colour = MRTClust), size = 3) +
   #geom_text_repel(data=Data.scores, aes(x=NMDS1, y=NMDS2, label=Site), size = 3) + 
   scale_shape_manual(values = c(18, 15, 16, 17, 20, 21, 19)) +
-  scale_color_manual(values = c("green3", "darkorange", "purple", "red", "black", "grey", "blue", "yellow")) +
-  #scale_color_manual(values = c("red", "darkgreen", "turquoise", "purple", "brown", "orange", "blue", "green", "grey81", "grey16", "yellow", "deeppink")) +
+  #scale_color_manual(values = c("green3", "darkorange", "purple", "red", "black", "grey", "blue", "yellow")) +
+  scale_color_manual(values = c("red", "darkgreen", "turquoise", "purple", "brown", "orange", "blue", "green", "grey81", "grey16", "yellow", "deeppink")) +
   annotate("text", x = -0.4, y = 0.6, label = paste("Stress = ", datanMDS$stress)) +
   ggtitle("SO") +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"))
 plot(plot)
+
 
 #==========================================================================================================================================
 # RDA
@@ -115,6 +178,7 @@ triplot.rda(PARTspe.rda,
             select.spe = PARTsel.sp
 )
 
+
 #==========================================================================================================================================
 #GLOBAL TEST FOR RDA & TEST CANONICAL AXES
 #==========================================================================================================================================
@@ -122,60 +186,3 @@ triplot.rda(PARTspe.rda,
 anova(spe.rda, permutations = how(nperm = 999))
 anova(spe.rda, by = "axis", permutations = how(nperm = 999))
 
-#==========================================================================================================================================
-# MRT : CONSTRAINED CLUSTERING
-#==========================================================================================================================================
-
-#ALL
-dev.new(
-  title = "Multivariate regression tree - all explanatory variables - ALL",
-  width = 14,
-  height = 7,
-  noRStudioGD = TRUE
-)
-par(mfrow = c(1, 2))
-spe.ch.mvpart <-
-  mvpart(
-    data.matrix(spe.norm) ~ .,
-    env,
-    margin = 0.08,
-    cp = 0,
-    xv = "pick",
-    xval = nrow(spe),
-    xvmult = 100
-  )
-
-summary(spe.ch.mvpart)
-printcp(spe.ch.mvpart)
-
-dev.new(
-  title = "Residuals of MRT",
-  width = 10,
-  height = 6,
-  noRStudioGD = TRUE
-)
-par(mfrow = c(1, 2))
-hist(residuals(spe.ch.mvpart), col = "bisque")
-plot(predict(spe.ch.mvpart, type = "matrix"),
-     residuals(spe.ch.mvpart),
-     main = "Residuals vs Predicted")
-abline(h = 0, lty = 3, col = "grey")
-
-
-spe.ch.mvpart.wrap <-
-  MRT(spe.ch.mvpart, percent = 10, species = colnames(spe))
-summary(spe.ch.mvpart.wrap)
-spe.ch.MRT.indval <- indval(spe.norm, spe.ch.mvpart$where)
-pval.adj3 <- p.adjust(spe.ch.MRT.indval$pval)
-spe.ch.MRT.indval$maxcls[which(pval.adj3 <= 0.05)]
-spe.ch.MRT.indval$indcls[which(pval.adj3 <= 0.05)]
-spech.mvpart.g <- factor(spe.ch.mvpart$where)
-levels(spech.mvpart.g) <- 1:length(levels(spech.mvpart.g))
-table(spech.mvpart.g, spech.UPGMA.g)
-
-dev.new(title = "MRT clusters",
-        width = 9,
-        noRStudioGD = TRUE)
-drawmap(xy = spa,
-        clusters = spech.mvpart.g,
-        main = "MRT clusters")
